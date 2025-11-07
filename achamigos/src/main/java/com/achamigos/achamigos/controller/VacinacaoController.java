ce555040e31d4392cf15db9458d773e30fb51756
Luan <luansilva_2@hotmail.com>
2025-11-06 21:47:09 -0300

    renderizando relatorio

--- achamigos/src/main/java/com/achamigos/achamigos/controller/VacinacaoController.java
+++ achamigos/src/main/java/com/achamigos/achamigos/controller/VacinacaoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/vacinacao")
 public class VacinacaoController {
